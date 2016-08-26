24d60a6088a7a6a8e4626c5c96a629b3d1135255
yekuigood <yekuigood@163.com>
2016-08-26 15:31:43 +0800

    netty-chat init

---
+++ src/main/java/org/example/netty/chat/ChatServerInitializer.java
@@ -0,0 +1,24 @@
+package org.example.netty.chat;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class ChatServerInitializer extends ChannelInitializer<SocketChannel> {
+
+	@Override
+	protected void initChannel(SocketChannel arg0) throws Exception {
+		ChannelPipeline pipeline = arg0.pipeline();
+		
+		pipeline.addLast("frame",new DelimiterBasedFrameDecoder(8192,Delimiters.lineDelimiter()));
+		pipeline.addLast("decoder",new StringDecoder());
+		pipeline.addLast("encoder",new StringEncoder());
+		
+		pipeline.addLast("handler",new ChatServerHandler());
+	}
+
+}
